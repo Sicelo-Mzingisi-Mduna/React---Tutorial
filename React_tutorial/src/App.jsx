85079fe95abb15555ba11320cb7e336be803d4dd
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-05-14 12:17:09 +0200

    React + Vite  Configuration

---
+++ React_tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [user_input, setUser_input] = useState([]);
+  const [submit, setSubmit] = useState([]);
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      
+    </>
+  )
+}
+
+export default App
